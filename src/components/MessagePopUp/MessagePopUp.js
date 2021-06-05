bfb4546794aaff2e42b658b0226f2efdbeba15db
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-06-05 10:14:07 -0700

    Added initial message popup

---
+++ src/components/MessagePopUp/MessagePopUp.js
@@ -0,0 +1,10 @@
+import './MessagePopUp.css'
+
+export default function MessagePopUp(props) {
+  return(
+    <div className="message-popup">
+      <h1>Message Title</h1>
+      <p>Message info...</p>
+    </div>
+  )
+}
