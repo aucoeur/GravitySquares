a86dfb4e150b5010b66c5a3b85f92e1da4c0d041
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-06-05 12:07:13 -0700

    rotate reducer

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 
-import {shapes} from './shapes'
+import { shapes, nextRotation, canMoveTo } from './shapes'
 
 export const random = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min
